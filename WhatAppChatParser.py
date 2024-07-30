537fdae914a3c449991f2727e187296316fd2fdd
Shehryar Sohail <hafizshehryar88@gmail.com>
2024-07-31 00:47:24 +0500

    Fix Regex and Retrieve Subjects and Messages

--- WhatAppChatParser.py
+++ WhatAppChatParser.py
@@ -1,28 +1,36 @@
 import re
-from typing import List
+from typing import List, Tuple
 
 class WhatsAppChatParser:
     def __init__(self, path: str):
-        self.path = path
-        self.chat_version = self._check_version()
+        self.path: str = path
+        
+        self._regex: str = r'(\d+/\d+/\d+, \d{1,2}:\d{2}\s*[APap][Mm]\s*-\s*)|\[\d+/\d+/\d+, \d{1,2}:\d{2}:\d{2} [APM]{2}\]'
+        self._extract_info: str = r"(?:\d{2}/\d{2}/\d{4},?\s+\d{1,2}:\d{2}(?:\s?[apAP][mM])?\s*-\s*|\[\d{4}/\d{1,2}/\d{1,2},?\s+\d{1,2}:\d{2}:\d{2}\s+[APM]{2}\]\s*|\[\d{2}/\d{2}/\d{4},?\s+\d{1,2}:\d{2}:\d{2}\s+[APM]{2}\]\s*)([^:]+):\s*(.*)"
+        
+        self.messages: List[str] = []
+        self.subject_list: List[str] = []
+        self.message_list: List[str] = []
+        self.subjects: set = set()
 
-        if self.chat_version == 'Android':
-            self._regex = r'\d+/\d+/\d+, \d{1,2}:\d{2}\s*[APap][Mm]\s*-\s*'
-        else:
-            self._regex = r'\[\d+/\d+/\d+, \d{1,2}:\d{2}:\d{2} [APM]{2}\]'
+        self._parse()
+        self._extract()
 
-        self.messages = self._parse()
+    def _extract(self):
+        for message in self.messages:
+            match = re.match(self._extract_info, message)
+            if match:
+                sender, text = match.groups()
+                self.subject_list.append(sender)
+                self.message_list.append(text)
 
-    def _check_version(self) -> str:
-        with open(self.path, 'r') as f:
-            texts = f.readlines()
-        return 'iOS' if texts[0][0] == '[' else 'Android'
+        self.subjects = set(self.subject_list)
     
-    def _parse(self) -> List[str]:
+    def _parse(self):
         with open(self.path, 'r', encoding='utf-8') as f:
             content = f.read()
 
-        # Remove all occurrences of \u200e and \u202f
+        # Remove all occurrences of \u200e
         content = content.replace('\u200e', '')
         content = content.replace('\u202f', ' ')
 
@@ -45,4 +53,7 @@ class WhatsAppChatParser:
         if current_message:
             messages.append(current_message)
 
-        return messages
+        self.messages = messages
+
+    def retrieve_subjects_and_messages(self) -> Tuple[List[str], List[str]]:
+        return self.subject_list, self.message_list
