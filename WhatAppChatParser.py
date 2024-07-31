bdc5114692eda7d6a14beb7f8d7e235c617b8e15
Shehryar Sohail <hafizshehryar88@gmail.com>
2024-07-31 16:11:20 +0500

    Add Functionalities

--- WhatAppChatParser.py
+++ WhatAppChatParser.py
@@ -1,8 +1,9 @@
 import re
+import pandas as pd
 from typing import List, Tuple
 
 class WhatsAppChatParser:
-    def __init__(self, path: str):
+    def __init__(self, path: str, turns: bool = True):
         self.path: str = path
         
         self._regex: str = r'(\d+/\d+/\d+, \d{1,2}:\d{2}\s*[APap][Mm]\s*-\s*)|\[\d+/\d+/\d+, \d{1,2}:\d{2}:\d{2} [APM]{2}\]'
@@ -15,6 +16,8 @@ class WhatsAppChatParser:
 
         self._parse()
         self._extract()
+
+        if turns: self._format_turns()
 
     def _extract(self):
         for message in self.messages:
@@ -55,5 +58,51 @@ class WhatsAppChatParser:
 
         self.messages = messages
 
+    def _format_turns(self):
+        subjects: List[str] = [self.subject_list[0]]
+        messages: List[str] = [self.message_list[0]]
+
+        current_subject: str = self.subject_list[0]
+        for subject, message in zip(self.subject_list[1:], self.message_list[1:]):
+            if subject != current_subject:
+                current_subject = subject
+                subjects.append(current_subject)
+                messages.append(message)
+            else:
+                messages[-1] += ' ' + message
+
+        self.subject_list = subjects
+        self.message_list = messages
+
     def retrieve_subjects_and_messages(self) -> Tuple[List[str], List[str]]:
         return self.subject_list, self.message_list
+    
+    def get_main_subject(self) -> str:
+        return self.subject_list[0]
+    
+    def set_main_subject(self, subject: str):
+        if subject not in self.subjects:
+            raise ValueError(f"Subject '{subject}' not found in the chat.")
+
+        if subject == self.get_main_subject():
+            return
+
+        self.message_list = self.message_list[1:]
+        self.subject_list = self.subject_list[1:]
+
+    def replace_subject(self, old_subject: str, new_subject: str):
+        if old_subject not in self.subjects:
+            raise ValueError(f"Subject '{old_subject}' not found in the chat.")
+
+        self.subject_list = [new_subject if subject == old_subject else subject for subject in self.subject_list]
+        
+    def to_frame(self) -> pd.DataFrame:
+        return pd.DataFrame(zip(self.subject_list, self.message_list), columns=['subject', 'message'])
+    
+    def to_csv(self, path: str) -> None:
+        df = self.to_frame()
+        df.to_csv(path, index=False)
+
+    def to_json(self, path: str) -> None:
+        df = self.to_frame()
+        df.to_json(path, orient='records', lines=True)
